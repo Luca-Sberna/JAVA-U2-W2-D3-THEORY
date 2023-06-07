0edf9d3e46af1e5c286466f559f03de7374e76fe
Luca-Sberna <sberna.luca.ibiza@gmail.com>
2023-06-07 13:17:53 +0200

    adj

--- src/main/java/U2W2D3THEORY/UsersRunner.java
+++ src/main/java/U2W2D3THEORY/UsersRunner.java
@@ -4,12 +4,14 @@ import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
 import com.github.javafaker.Faker;
 
 import users.User;
 import users.UsersService;
 
+@Component
 public class UsersRunner implements CommandLineRunner {
 	@Autowired
 	UsersService usersService;
@@ -17,13 +19,19 @@ public class UsersRunner implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		Faker faker = new Faker(new Locale("it"));
-		for (int i = 0; i < 20; i++) {
-			String name = faker.name().firstName();
-			String surname = faker.name().lastName();
-			String email = faker.internet().emailAddress();
-			User user = new User(name, surname, email);
-			usersService.create(user);
+		for (int i = 0; i < 3; i++) {
+			try {
+
+				String name = faker.name().firstName();
+				String surname = faker.name().lastName();
+				String email = faker.internet().emailAddress();
+				User user = new User(name, surname, email);
+				// usersService.create(user);
+			} catch (Exception e) {
+				System.out.println(e);
+			}
 		}
+
 	}
 
 }
